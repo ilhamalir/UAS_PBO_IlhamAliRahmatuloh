521b1357df89cf28b83338d5c80fdf058ca8fe6c
zaidan321 <mhafizzaidan@gmail.com>
2026-05-26 10:52:12 +0700

    first commit

---
+++ Access_Modifier.php
@@ -0,0 +1,21 @@
+<?php
+class Fruit {
+  protected $name;
+
+  public function setType($name) {
+    $this->name = $name;
+  }
+}
+
+class Apple extends Fruit {
+  public function getType() {
+    echo "Name: " . $this->name . ".";
+  }
+}
+
+$apple = new Apple();
+$apple->setType("Apple");
+//echo $apple->name; // Error: Cannot access protected property
+echo $apple->getType(); // Output: Name: Apple.
+?>
+
